120636324b94b1f78da91c8bbd0ede45a213b086
zhangkun25 <11126591@qq.com>
2020-06-14 16:11:23 +0800

    v0.7

--- src/main/java/com/git/web/GitTestController.java
+++ src/main/java/com/git/web/GitTestController.java
@@ -8,6 +8,6 @@ public class GitTestController {
 
     @RequestMapping("/git")
     public String git(){
-        return "hello git -v0.6 master update";
+        return "hello git -v0.6 master update merge 合并分支";
     }
 }
